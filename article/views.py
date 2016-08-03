7ea85ff689c0ba7a10a4d2b58e0475fe2c1c8842
apple <apple@appledeMacBook-Pro-2.local>
2016-08-04 00:06:06 +0800

    paginator

--- article/views.py
+++ article/views.py
@@ -2,18 +2,58 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def home(request):
-	post_list = Article.objects.all()
+	posts = Article.objects.all()
+	paginator = Paginator(posts, 2)
+	page = request.GET.get('page')
+	try:
+		post_list = paginator.page(page)
+	except PageNotAnInteger:
+		post_list = paginator.page(1)
+	except EmptyPage:
+		post_list = paginator.paginator(paginator.num_pages)
 	return render(request, 'home.html', {'post_list': post_list})
-	return HttpResponse("Hello World, Django")
 
-def detail(request, my_args):
-	post = Article.objects.all()[int(my_args)]
-	str = ("title = %s, category = %s, date_time = %s, content = %s"
-		% (post.title, post.category, post.date_time, post.content))
-	return HttpResponse(str)
+def detail(request, id):
+	try:
+		post = Article.objects.get(id = str(id))
 
-#def test(request):
-#	return render(request, 'test.html', {'current_time': datetime.now()})
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'post.html', {'post': post})
+
+def archives(request):
+	try:
+		post_list = Article.objects.all()
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def search_tag(request, tag):
+	try:
+		post_list = Article.objects.filter(category__iexact = tag)
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'tag.html', {'post_list': post_list})
+
+def blog_search(request):
+	if 's' in request.GET:
+		s = request.GET['s']
+		if not s:
+			return render(request, 'home.html')
+		else:
+			post_list = Article.objects.filter(title__icontains = s)
+			if len(post_list) == 0:
+				return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+			else:
+				return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+	return redirect('/')
+
+
